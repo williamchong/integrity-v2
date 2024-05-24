b414233173b225d82ed3f599e47d89a9cf8f3e8d
William Chong <me@williamchong.cloud>
2024-05-25 04:27:25 +0800

    [preprocessor/folder] add project metadata database

--- preprocessor/folder/database_schema.go
+++ preprocessor/folder/database_schema.go
@@ -15,3 +15,14 @@ var FILE_STATUS_TABLE = `CREATE TABLE IF NOT EXISTS file_status (
 	CREATE INDEX IF NOT EXISTS idx_file_status_sha256 ON file_status (sha256);
 	CREATE INDEX IF NOT EXISTS idx_file_status_status ON file_status (status);
 `
+
+var PROJECT_METADATA_TABLE = `CREATE TABLE IF NOT EXISTS project_metadata (
+	id BIGSERIAL PRIMARY KEY,
+	project_id TEXT UNIQUE NOT NULL,
+	project_path TEXT UNIQUE NOT NULL,
+	author_type TEXT,
+	author_name TEXT,
+	author_identifier TEXT
+);
+CREATE INDEX IF NOT EXISTS idx_project_metadata_project_id ON project_metadata (project_id);
+`
